6b1660a5ccbd7d96db80c83332d5b0fb0994cdb9
Adam Alfredsson <alfredsson.adam@gmail.com>
2020-04-08 15:12:55 +0200

    fix functions

---
+++ functions/src/register.ts
@@ -0,0 +1,31 @@
+import querystring from 'querystring';
+import axios from 'axios';
+
+const { GOOGLE_SCRIPT_URL, SLACK_WEBHOOK_URL } = process.env;
+
+export const handler = async (event: any, context: any) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const params = querystring.parse(event.body);
+  const email = params.email.toString();
+
+  await saveInGoogleSheets(email);
+  await sendSlackMessage(email);
+
+  return {
+    statusCode: 200,
+    body: `Hello, ${name}! Your greeting has been sent to Slack 👋`,
+  };
+};
+
+const saveInGoogleSheets = async (email: string): Promise<void> => {
+  await axios.post(GOOGLE_SCRIPT_URL!, { email });
+};
+
+const sendSlackMessage = async (email: string): Promise<void> => {
+  await axios.post(SLACK_WEBHOOK_URL!, {
+    text: `${email} says hello!`,
+  });
+};
