ce369a9fb294c1b19c79dc619b869d41b1c67eaf
Adam Alfredsson <alfredsson.adam@gmail.com>
2020-04-09 10:40:46 +0200

    fix signup function

--- frontend/src/components/Hero/index.tsx
+++ frontend/src/components/Hero/index.tsx
@@ -3,12 +3,26 @@ import { StyledHero } from './style';
 import { Device } from '../Device';
 import { ReactComponent as Logo } from '../../assets/icons/logo.svg';
 import frame from '../../assets/img/frame.jpg';
+import axios from 'axios';
 
 export const Hero: React.FC = () => {
-  const [email, setEmail] = useState<string | undefined>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [email, setEmail] = useState<string>('');
 
-  const onFormSubmit = (event: FormEvent) => {
+  const endpoint = process.env.REACT_APP_API_URL! + '/signup';
+
+  const onFormSubmit = async (event: FormEvent) => {
     event.preventDefault();
+    setIsLoading(true);
+    await sendSignupRequest();
+    setIsFormSubmitted(true);
+    setIsLoading(true);
+  };
+
+  const sendSignupRequest = async () => {
+    console.log('SIGN UP', email);
+    return axios.post(endpoint, { email });
   };
 
   return (
@@ -21,22 +35,28 @@ export const Hero: React.FC = () => {
         </nav>
         <article>
           <h2>Virtuella cook-alongs</h2>
-          <p>Följ receptet live och ha kul samtidigt som du blir en bättre kock</p>
-          <form onSubmit={onFormSubmit}>
-            <label>
-              <p>Bli först med att använda:</p>
-              <input
-                type="email"
-                placeholder="johanna@gmail.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </label>
-            <button>Skicka!</button>
-          </form>
+          <p>Följ receptet live och ha kul samtidigt som du blir en bättre kock {endpoint}</p>
+          {isFormSubmitted ? (
+            <p>Tack så mycket!</p>
+          ) : (
+            <form onSubmit={onFormSubmit}>
+              <label>
+                <p>Bli först med att använda:</p>
+                <input
+                  type="email"
+                  placeholder="johanna@gmail.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
+              <button type="submit" disabled={isLoading}>
+                Skicka!
+              </button>
+            </form>
+          )}
         </article>
         <Device device="iPhoneX" color="black" orientation="portrait">
-          <img src={frame} alt="A preview of the app" />
+          <img src={frame} alt="Chopalong" />
         </Device>
       </main>
     </StyledHero>
--- functions/src/signup.ts
+++ functions/src/signup.ts
@@ -1,9 +1,17 @@
-import querystring from 'querystring';
 import axios from 'axios';
 
 const { GOOGLE_SCRIPT_URL, SLACK_WEBHOOK_URL } = process.env;
 
 export const handler = async (event: any, context: any) => {
+  if (event.httpMethod === 'OPTIONS') {
+    const headers = {
+      'Access-Control-Allow-Origin': 'https://chopalong.se',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+      'Access-Control-Allow-Methods': 'POST',
+      'Content-Type': 'application/json',
+    };
+    return { statusCode: 200, headers, body: 'OK' };
+  }
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
@@ -18,7 +26,7 @@ export const handler = async (event: any, context: any) => {
 
   return {
     statusCode: 200,
-    body: `Hello, ${name}! Your greeting has been sent to Slack 👋`,
+    body: `Thank you! 👋`,
   };
 };
 
@@ -28,6 +36,6 @@ const saveInGoogleSheets = async (email: string): Promise<void> => {
 
 const sendSlackMessage = async (email: string): Promise<void> => {
   await axios.post(SLACK_WEBHOOK_URL!, {
-    text: `${email} says hello!`,
+    text: `${email} says hello! 👋`,
   });
 };
